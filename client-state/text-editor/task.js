2dc05769a622d7db81c20e3eaae3c908237bb3db
SashaT29 <tusnetovaa@gmail.com>
2023-09-20 10:39:48 +0300

    Задание 3. Удалила кнопку

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,9 +2,3 @@ const texrArea = document.getElementById('editor');
 
 texrArea.value = localStorage.text;
 texrArea.oninput = () => localStorage.text = texrArea.value;
-
-const clearBtn = document.getElementById('clearBtn');
-
-clearBtn.addEventListener('click', function() {
-    localStorage.clear();
-});
